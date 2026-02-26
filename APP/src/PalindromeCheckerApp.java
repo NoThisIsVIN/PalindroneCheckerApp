be6dff034ff90e510b4590c5b6b55af64e7bec8f
NoThisIsVIN <vineshreddy2007@gmail.com>
2026-02-26 20:16:05 +0530

    UC1 final

---
+++ APP/src/PalindromeCheckerApp.java
@@ -0,0 +1,8 @@
+import java.util.*;
+
+public class UseCase13PalindromeCheckerApp {
+
+    public static void main(String[] args) {
+        system.out.println("Welcome to the Palindrome Checker App!");
+    }
+}
