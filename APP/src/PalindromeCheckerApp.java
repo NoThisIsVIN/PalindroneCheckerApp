a3445572483b957cbdfd54edb06d8abdb0e92abd
NoThisIsVIN <vineshreddy2007@gmail.com>
2026-02-26 15:23:23 +0530

    u2

---
+++ APP/src/PalindromeCheckerApp.java
@@ -0,0 +1,15 @@
+class UseCase2PalindromeCheckerApp {
+
+    public static void main(String[] args) {
+        String str = "madam";
+        String reverse = "";
+        for (int i = str.length() - 1; i >= 0; i--) {
+            reverse = reverse + str.charAt(i);
+        }
+        if (str.equals(reverse)) {
+            System.out.println(str + " is a Palindrome");
+        } else {
+            System.out.println(str + " is NOT a Palindrome");
+        }
+    }
+}
