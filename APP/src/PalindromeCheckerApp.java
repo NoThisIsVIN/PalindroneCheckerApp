b209aad9e128bc40f946e72c81a2528a94503402
NoThisIsVIN <vineshreddy2007@gmail.com>
2026-02-26 16:31:30 +0530

    uc9

--- APP/src/PalindromeCheckerApp.java
+++ APP/src/PalindromeCheckerApp.java
@@ -1,68 +1,15 @@
 import java.util.Scanner;
 
-public class UseCase8PalindromeCheckerApp {
+public class UseCase9PalindromeCheckerApp {
 
-    static class Node {
-        char data;
-        Node next;
-
-        Node(char data) {
-            this.data = data;
-            this.next = null;
-        }
-    }
-
-    static Node head;
-
-    static void insert(char data) {
-        Node newNode = new Node(data);
-        if (head == null) {
-            head = newNode;
-            return;
-        }
-        Node temp = head;
-        while (temp.next != null) {
-            temp = temp.next;
-        }
-        temp.next = newNode;
-    }
-
-    static Node reverse(Node node) {
-        Node prev = null;
-        Node current = node;
-        while (current != null) {
-            Node nextNode = current.next;
-            current.next = prev;
-            prev = current;
-            current = nextNode;
-        }
-        return prev;
-    }
-
-    static boolean isPalindrome() {
-        if (head == null || head.next == null) {
+    static boolean checkPalindrome(String str, int start, int end) {
+        if (start >= end) {
             return true;
         }
-
-        Node slow = head;
-        Node fast = head;
-
-        while (fast.next != null && fast.next.next != null) {
-            slow = slow.next;
-            fast = fast.next.next;
+        if (str.charAt(start) != str.charAt(end)) {
+            return false;
         }
-
-        Node secondHalf = reverse(slow.next);
-        Node firstHalf = head;
-
-        while (secondHalf != null) {
-            if (firstHalf.data != secondHalf.data) {
-                return false;
-            }
-            firstHalf = firstHalf.next;
-            secondHalf = secondHalf.next;
-        }
-        return true;
+        return checkPalindrome(str, start + 1, end - 1);
     }
 
     public static void main(String[] args) {
@@ -72,11 +19,7 @@ public class UseCase8PalindromeCheckerApp {
         System.out.print("Enter a string: ");
         String input = scanner.nextLine();
 
-        for (int i = 0; i < input.length(); i++) {
-            insert(input.charAt(i));
-        }
-
-        if (isPalindrome()) {
+        if (checkPalindrome(input, 0, input.length() - 1)) {
             System.out.println("The given string is a Palindrome.");
         } else {
             System.out.println("The given string is NOT a Palindrome.");
