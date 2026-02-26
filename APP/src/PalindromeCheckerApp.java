fc8b36091af03037c23be2866b027fa18f2444c2
NoThisIsVIN <vineshreddy2007@gmail.com>
2026-02-26 16:21:58 +0530

    u6

--- APP/src/PalindromeCheckerApp.java
+++ APP/src/PalindromeCheckerApp.java
@@ -1,11 +1,14 @@
 import java.util.Scanner;
+import java.util.Queue;
+import java.util.LinkedList;
 import java.util.Stack;
 
-public class UseCase5PalindromeCheckerApp {
+public class UseCase6PalindromeCheckerApp {
 
     public static void main(String[] args) {
 
         Scanner scanner = new Scanner(System.in);
+        Queue<Character> queue = new LinkedList<>();
         Stack<Character> stack = new Stack<>();
 
         System.out.print("Enter a string: ");
@@ -13,14 +16,16 @@ public class UseCase5PalindromeCheckerApp {
 
 
         for (int i = 0; i < input.length(); i++) {
-            stack.push(input.charAt(i));
+            char ch = input.charAt(i);
+            queue.add(ch);
+            stack.push(ch);
         }
 
         boolean isPalindrome = true;
 
 
-        for (int i = 0; i < input.length(); i++) {
-            if (input.charAt(i) != stack.pop()) {
+        while (!queue.isEmpty()) {
+            if (queue.remove() != stack.pop()) {
                 isPalindrome = false;
                 break;
             }
